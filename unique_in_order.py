be5a1dd5edb5dd434b6a114d8352de02ffb8698b
pure.struggle <mattsburke@gmail.com>
2017-04-13 00:42:15 -0400

    unique_in_order commented/final

---
+++ unique_in_order.py
@@ -0,0 +1,20 @@
+#Implement the function unique_in_order which takes as argument a sequence and returns a list of items 
+#without any elements with the same value next to each other and preserving the original order of 
+#elements.
+
+#For example:
+
+#unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+#unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+#unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+def unique_in_order(iterable):
+    a=[]
+    x=''
+    #check if last char was different. if so, append.
+    for char in iterable:
+   		if x != char:
+			a.append(char)
+		x=char
+         
+    return a
